d6c43a63a85d1030bc6adba7dae4ceb07eea4eb8
Devtel0912 <devtel0912@gmail.com>
2025-11-04 11:28:04 -0500

    Currency Converter

---
+++ main.py
@@ -0,0 +1,61 @@
+from optparse import Values
+import tkinter as tk
+from forex_python.converter import CurrencyRates
+
+
+common_currencies = ["USD","CAD","EUR","GBP","JPY","AUD","CHF","NZD"]
+
+
+
+class Converter:
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Currency Converter")
+        self.root.geometry('200x180')
+
+        self.from_var = tk.StringVar(self.root)
+        self.from_var.set("USD")
+        self.from_menu = tk.OptionMenu(self.root, self.from_var, *common_currencies)
+        self.from_menu.pack(pady=1)
+
+
+        self.from_var = tk.StringVar(self.root)
+        self.from_var.set("EUR")
+        self.from_menu = tk.OptionMenu(self.root, self.from_var, *common_currencies)
+        self.from_menu.pack(pady=1)
+
+
+
+        self.amount_label = tk.Label(self.root, text="Amount: ")
+        self.amount_label.pack(pady=1)
+
+
+        self.amount_entry = tk.Entry(self.root)
+        self.amount_entry.pack(pady=1)
+
+
+        self.convert = tk.Button(self.root, text="Convert", command=self.convertCurrency)
+        self.convert.pack(pady=1)
+
+
+        self.result_label = tk.Label(self.root,text="")
+        self.result_label.pack(pady=1)
+
+        self.root.mainloop()
+
+        
+    def convertCurrency(self):
+        try:
+            from_currency = self.from_var.get()
+            to_currency = self.from_var.get()
+            amount = float(self.amount_entry.get())
+            c = CurrencyRates()
+            result = c.convert(from_currency, to_currency, amount)
+            self.result_label.config(text=f"{amount:.2f} {from_currency} = {result:.2f} {to_currency}")
+        except Exception as e:
+            self.result_label.config(text="Error: Invalid input")
+
+
+if __name__ == "__main__":
+    Converter()
